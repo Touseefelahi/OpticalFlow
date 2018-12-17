b03ad827ea98dbc3667d4a0ecb6423938b6d684d
Touseef <touseef07@gmail.com>
2018-12-17 10:16:11 +0300

    Uploading sample edge detection program

---
+++ visionworks.cpp
@@ -0,0 +1,5 @@
+#include "visionworks.h"
+
+VisionWorks::VisionWorks()
+{
+}
---
+++ visionworks.h
@@ -0,0 +1,10 @@
+#ifndef VISIONWORKS_H
+#define VISIONWORKS_H
+
+class VisionWorks
+{
+public:
+    VisionWorks();
+};
+
+#endif // VISIONWORKS_H
